e7cfadc33783d341934e447e4d8fcf797ce9dbc2
Tho.Ngo <nhochamvui.love@gmail.com>
2022-03-10 21:22:57 +0700

    - add new config for deployment

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,5 @@ module.exports = {
             cert: fs.readFileSync('./certs/dev_localhost.pem'),
         },
     },
-    publicPath: process.env.NODE_ENV === 'production' ? '/tinderclonev2/' : '/'
+    publicPath: process.env.DEPLOY_SITE === 'netlify' && process.env.NODE_ENV === 'production' ? '/' : '/tinderclonev2/'
 }
