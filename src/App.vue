ad3c7fd25b741739959ed833f56b213bf341b0df
Tho Hoang Ngo <nhochamvui.love@gmail.com>
2026-09-05 21:27:15 +0700

    Show register page instead of Loading (add SimpleSignup to App allowlist)

--- src/App.vue
+++ src/App.vue
@@ -24,6 +24,7 @@
                     !isLoading ||
                     routeName === 'Authenticate' ||
                     routeName === 'SignupProfile' ||
+                    routeName === 'SimpleSignup' ||
                     routeName === 'Privacy'"
             >
                 <router-view/>
